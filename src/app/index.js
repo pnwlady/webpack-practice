34a07218fbd8b122677ee2858266247384565a61
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-07-13 15:51:45 -0700

    can't get the template in js to work

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,14 @@
 import angular from 'angular';
-// import template from './index.tpl.html';
+// template: require('./index.tpl.html'
 import '../style/app.scss';
-
+//
 // let component = {
 //     template // Use ES6 enhanced object literals.
 // }
 //
 // let app = angular.module('app', [])
 //     .component('app', component)
-
+//
 // class TestClass {
 //     constructor() {
 //         let msg = "Using ES2015+ syntax";
