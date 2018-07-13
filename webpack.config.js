4d8a6d820126704152b80320f4c884d674adacda
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-07-13 10:26:08 -0700

    webpack config with bundle path, html plugin and dev server set up

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + "/src/app/index.js",
+    output:  {
+        path: __dirname + '/dist', //Folder to store bundle generated
+        filename: 'bundle.js', //generated budle filename
+        publicPath: '/' //public URL of the output directory when referenced in a browser
+    },
+    module: {
+        rules: [
+
+        ]
+    },
+    plugins: [ // array of plugins to apply to build chunk
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/public/index.html",
+            inject: 'body'
+        })
+    ],
+    devServer: { //config webpack-dev-server
+        contentBase: './src/public', //sorce of static assets
+        port: 1111, //port running dev-server
+    }
+};
